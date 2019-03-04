fa3de9a8321e8ce8fe23e51cd835bd507ca9de24
Ihor Stryzhnow <thetotemofficial@gmail.com>
2019-03-04 14:25:43 +0200

    Add new project about personge

---
+++ AppPersonage/AppPersonage/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPersonage
+{
+    class Person
+    {
+        public string name;
+        public string surname;
+        public int age;
+
+        public Person()
+        {
+            name = "Tom";
+            surname = "Johnson";
+            age = 18;
+        }
+        public Person(string _name, string _surname, int _age)
+        {
+            name = _name;
+            surname = _surname;
+            age = _age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name {name} Surname {surname} Age {age}";
+        }
+
+
+    }
+    
+}
---
+++ AppPersonage/AppPersonage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPersonage
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int value = Convert.ToInt32(Console.ReadLine());
+            Person [] array = new Person [value];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Input name {i} person");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Input surname {i} person");
+                string surname = Console.ReadLine();
+                Console.WriteLine($"Input age {i} person");
+                int age = Convert.ToInt32(Console.ReadLine());
+                array[i] = new Person(name, surname, age);
+            }
+           for (int i = 0; i < array.Length; i++)
+            {
+                
+                //Console.WriteLine($"Name {array[i].name} Surname {array[i].surname} Age {array[i].age}");
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
